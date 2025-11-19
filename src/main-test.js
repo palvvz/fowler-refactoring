ec95c8832bcd5925c90c82eca16eca8661fb5caa
palvvz <palvvz@gmail.com>
2025-11-20 03:28:01 +0700

    refactoring

--- src/main-test.js
+++ src/main-test.js
@@ -5,10 +5,17 @@ const fs=require('fs');
 main();
 
 function main() {
-const invoice = JSON.parse(fs.readFileSync('test/invoice.json', 'utf8'));
-  const plays = JSON.parse(fs.readFileSync('test/plays.json', 'utf8'));
-    let res = statement(invoice, plays);
-    console.log(res);
+// const invoice = JSON.parse(fs.readFileSync('test/invoice.json', 'utf8'));
+//   const plays = JSON.parse(fs.readFileSync('test/plays.json', 'utf8'));
+//     let res = statement(invoice, plays);
+//     console.log(res);
+
+const obj = {str :[]};
+let amount=1,credits=2;
+obj.str.push({ amount, credits });
+amount=3,credits=24;
+obj.str.push({ amount, credits });
+t = 1;
 }
 
 
--- src/statement.js
+++ src/statement.js
@@ -1,43 +1,33 @@
+const createStatementData = require("./createStatementData");
 
 function statement (invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-            minimumFractionDigits: 2 }).format;
+    const statementData = createStatementData(invoice, plays);
+    return renderPlainText(statementData);
+}
 
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = 0;
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+function renderPlainText(data) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
         // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        result += ` ${perf.play.name}: ${(usd(perf.amount))} (${perf.audience} seats)\n`;
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
+    result += `You earned ${(data.totalVolumeCredits)} credits\n`;
     return result;
+
+    function usd(number) {
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency", currency: "USD",
+                minimumFractionDigits: 2
+            }).format(number / 100);
+    }
+
+}
+
+function renderHtml(data) {
+    // TODO
 }
 
 module.exports = statement;
